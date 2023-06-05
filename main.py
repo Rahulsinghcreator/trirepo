e0f8a7b8ff2804d9bb5f218fd6a76129e042caaa
Pratik-Botz <130047132+Pratik-Botz@users.noreply.github.com>
2023-06-05 11:22:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ import os
 
 bot = Client(
     "bot",
-    bot_token= "6094487944:AAFeK0Z_OHTYATZEuX1QkXe3Tq-Z8Uh41cE",
+6077054491:AAHZvYzs1Hdv59edzpVKc0gxoxeqvrA_f-E",
     api_id= 29410389,
     api_hash= "0c716764715886f6641477ffbb63e1ee")
 
